71798ef8adfe76bc16bc4d8d268a4499068c1e56
skr1pmen <KiberKot.v.roymenge@gmail.com>
2024-04-11 18:59:05 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<head>
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="./assets/styles/index.css">
+</head>
+<header>
+    <div class="container">
+        <a href="/">LibraryOnline</a>
+        <nav>
+            <a href="./pages/auth.php">Войти</a>
+            <a href="./pages/reg.php">Регистрация</a>
+        </nav>
+    </div>
+</header>
+<main>
+
+</main>
+<footer>
+    ©LibraryOnline <?= Date("Y") ?>
+</footer>
