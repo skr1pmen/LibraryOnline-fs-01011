ee420d95be8caa75834d0d50c2764222ad88a3b0
skr1pmen <KiberKot.v.roymenge@gmail.com>
2024-04-16 20:25:15 +0400

    comment added

--- backend/db.php
+++ backend/db.php
@@ -1,11 +1,13 @@
 <?php
+
+//Создание констант для дальнейшего общения с базой данных
 const DB_PROVIDER = 'pgsql';
 const DB_HOST = 'localhost';
 const DB_BASENAME = 'libraryonline';
 const DB_USERNAME = 'postgres';
 const DB_PASSWORD = '';
 
-function db_connect()
+function db_connect() //Функция для соединения с базой данных
 {
     $pdo = new PDO(
         DB_PROVIDER . ':host=' . DB_HOST . ';dbname=' . DB_BASENAME,
@@ -15,7 +17,27 @@ function db_connect()
     return $pdo;
 }
 
-function query($sql, $params = [])
+function query($sql, $params = []) //Функция для отправки запросов
+{
+    $pdo = db_connect(); //Получение соединения с бд
+    $que = $pdo->prepare($sql); //Подготовка запроса
+    if (!empty($params)) { //Проверка доп.параметров на существование
+        foreach ($params as $key => $value) { //Перебор доп.параметров
+            $que->bindValue(':' . $key, $value); //Внедрение параметров в запрос
+        }
+    }
+    $que->execute(); //Отправка запроса
+    return $que; //Возвращаем результат запроса
+}
+
+function select($sql, $params = []) //Функция для получения данных из бд
+{
+    $que = query($sql, $params); //Отправка запроса для получения данных
+    $que->setFetchMode(PDO::FETCH_ASSOC); //Преобразование полученных данных в ассоциативный массив
+    return $que->fetchAll(); //Получение всех данных из запроса
+}
+
+function insert($sql, $params = []) //Функция добавления новых данных
 {
     $pdo = db_connect();
     $que = $pdo->prepare($sql);
@@ -25,37 +47,17 @@ function query($sql, $params = [])
         }
     }
     $que->execute();
-    return $que;
+    return $pdo->lastInsertId(); //Получение id последней записи
 }
 
-function select($sql, $params = [])
+function update($sql, $params = []) //Функция изменения данных
 {
-    $que = query($sql, $params);
-    $que->setFetchMode(PDO::FETCH_ASSOC);
-    return $que->fetchAll();
+    $que = query($sql, $params); //Отправка запроса для получения данных
+    return $que; //Возвращаем результат операции
 }
 
-function insert($sql, $params = [])
+function delete($sql, $params = []) //Функция удаления данных
 {
-    $pdo = db_connect();
-    $que = $pdo->prepare($sql);
-    if (!empty($params)) {
-        foreach ($params as $key => $value) {
-            $que->bindValue(':' . $key, $value);
-        }
-    }
-    $que->execute();
-    return $pdo->lastInsertId();
-}
-
-function update($sql, $params = [])
-{
-    $que = query($sql, $params);
-    return $que;
-}
-
-function delete($sql, $params = [])
-{
-    $que = query($sql, $params);
-    return $que;
+    $que = query($sql, $params); //Отправка запроса для получения данных
+    return $que; //Возвращаем результат операции
 }
--- backend/registration.php
+++ backend/registration.php
@@ -1,11 +1,11 @@
 <?php
 //var_dump($_POST);
 //var_dump($_FILES);
-require './db.php';
+require './db.php'; // Подключения файла для взаимодействия с бд
 
-$fio = $_POST['fio'];
-$login = $_POST['login'];
-$password = $_POST['password'];
-$avatar = $_FILES['avatar'];
+$fio = $_POST['fio']; //Создание переменной для ФИО пользователя
+$login = $_POST['login']; //Создание переменной для ЛОГИНА пользователя
+$password = $_POST['password']; //Создание переменной для ПАРОЛЯ пользователя
+$avatar = $_FILES['avatar']; //Создание переменной для АВАТАРКИ пользователя
 
 var_dump($fio, $login, $password, $avatar);
